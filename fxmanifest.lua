ddbda97dc940c381bee782f73bdd70aaa9ad6d93
Fyta <88390051+Fyta0@users.noreply.github.com>
2025-02-22 14:58:38 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
 
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua'
+}
