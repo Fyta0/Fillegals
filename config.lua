5ff8701a575c74541b5010ecd0830bc744cabaf1
Fyta <88390051+Fyta0@users.noreply.github.com>
2025-02-22 14:57:41 +0100

    config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+ 
+Config.MarkerColor = {r = 0, g = 200, b = 204}
+Config.JobName = "police" -- required  job
+
+Config.CountMoney = 300
+Config.BonusMoney = 0
+
+Config.Items = {"water","bread","black_money"} -- List of the illegal items (do not add weapons!)
+
+Config.Zones = { 
+    {type = 22, x = 1114.8081, y = -2006.6953, z = 35.4393} -- script zones
+}
